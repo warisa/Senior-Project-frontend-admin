3e7e68fb2f2d95697f288cfe98935c996375f5d0
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-10-18 18:33:25 +0700

    [EDIT] Design web page

--- muslims-app/src/page/index.js
+++ muslims-app/src/page/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Appbar from '../component/Appbar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import "../css/Index.css";
 
 export default class index extends Component {
   constructor(props) {
@@ -9,7 +14,29 @@ export default class index extends Component {
 
   render() {
     return (
-        <div>index</div>
+        <div className="index">
+          <Appbar/>
+          <br/>
+          <Grid container direction="column" alignItems="center" >
+            <Grid item xs={12} md={6}>
+            <ButtonGroup
+              variant="contained"
+              color="primary"
+              size="large"
+              aria-label="large contained secondary button group"
+            >
+                <Button style={{backgroundColor:'#CC6633'}}>Restaurant</Button>
+                <Button style={{backgroundColor:'#CC6633'}}>Pray Place</Button>
+                <Button style={{backgroundColor:'#CC6633'}}>History</Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+          <center>
+          <header className="index-header">
+                <img src="../images/logo-web.png" className="index-logo"  alt="index_logo" />
+            </header>
+            </center>
+        </div>
     );
   }
 }
