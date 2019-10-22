738cb70f48e439b27cd529e4397bdd50163f6601
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-10-22 19:54:35 +0700

    [DELETE] Delete history button

--- muslims-app/src/page/index.js
+++ muslims-app/src/page/index.js
@@ -25,7 +25,7 @@ function index () {
             >
                 <Button onClick={()=>{changePage('/restaurant')}} style={{backgroundColor:'#CC6633'}}>Restaurant</Button>
                 <Button onClick={()=>{changePage('/prayplace')}} style={{backgroundColor:'#CC6633'}}>Pray Place</Button>
-                <Button onClick={()=>{changePage('/')}} style={{backgroundColor:'#CC6633'}}>History</Button>
+                {/* <Button onClick={()=>{changePage('/')}} style={{backgroundColor:'#CC6633'}}>History</Button> */}
               </ButtonGroup>
             </Grid>
           </Grid>
